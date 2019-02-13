ecd069ab65a82173a19305bfc5c39f755205864b
Alex McCaskey <mccaskeyaj@ornl.gov>
2019-02-13 19:30:01 +0000

    adding some comments
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- src/driver-rpc/mpi/tests/client.cpp
+++ src/driver-rpc/mpi/tests/client.cpp
@@ -6,18 +6,28 @@ int main(int argc, char** argv) {
 
   MPI_Init(&argc, &argv);
 
+  // Create the client
   MPIClient client;
+
+  // Send some taprol asynchronously, here its just
+  // a test so just send any string
   auto jobId = client.sendTaProl("hello world");
+
   std::cout << "[client.cpp] job-id = " << jobId << ".\n";
 
+  // Retrieve the result
   auto value = client.retrieveResult(jobId);
 
   std::cout << "[client.cpp] value is " << value << "\n";
 
+  // Try it again!
   jobId = client.sendTaProl("hello again");
   value = client.retrieveResult(jobId);
+
   std::cout << "[client.cpp] value second time is " << value << "\n";
 
+  // Shutdown the client, this
+  // also tells the server to shutdown.
   client.shutdown();
 
   MPI_Finalize();
--- src/driver-rpc/mpi/tests/server.cpp
+++ src/driver-rpc/mpi/tests/server.cpp
@@ -6,9 +6,15 @@ int main(int argc, char** argv) {
 
     MPI_Init(&argc,&argv);
 
+    // Create the server and
+    // start it up, this kicks off the
+    // event loop
     MPIServer server;
     server.start();
 
+    // If we make it here, then the
+    // client has shutdown the server
+    
     std::cout << "Exiting server.cpp\n";
 
     MPI_Finalize();
