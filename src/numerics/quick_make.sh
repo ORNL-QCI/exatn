2667f4f3f7b6eb1c37ed04e3b21a140d98f7d50b
Dmitry I. Lyakh <quant4me@gmail.com>
2019-02-15 18:01:07 -0500

    Added fPIC to quick make.

--- src/numerics/quick_make.sh
+++ src/numerics/quick_make.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-g++ -c -g -std=c++14 basis_vector.cpp
-g++ -c -g -std=c++14 space_basis.cpp
-g++ -c -g -std=c++14 spaces.cpp
-g++ -c -g -std=c++14 tensor_signature.cpp
-g++ -c -g -std=c++14 tensor_shape.cpp
-g++ -c -g -std=c++14 tensor_leg.cpp
-g++ -c -g -std=c++14 tensor.cpp
-g++ -c -g -std=c++14 tensor_exa.cpp
-g++ -c -g -std=c++14 tensor_factory.cpp
-g++ -c -g -std=c++14 register.cpp
+rm *.o
+g++ -c -g -fPIC -std=c++14 basis_vector.cpp
+g++ -c -g -fPIC -std=c++14 space_basis.cpp
+g++ -c -g -fPIC -std=c++14 spaces.cpp
+g++ -c -g -fPIC -std=c++14 tensor_signature.cpp
+g++ -c -g -fPIC -std=c++14 tensor_shape.cpp
+g++ -c -g -fPIC -std=c++14 tensor_leg.cpp
+g++ -c -g -fPIC -std=c++14 tensor.cpp
+g++ -c -g -fPIC -std=c++14 tensor_exa.cpp
+g++ -c -g -fPIC -std=c++14 tensor_factory.cpp
+g++ -c -g -fPIC -std=c++14 register.cpp
