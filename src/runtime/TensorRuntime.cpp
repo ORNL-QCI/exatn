b6577d5b1ab15537db1eb858f58b5ec9d6e1bacc
Dmitry I. Lyakh <quant4me@gmail.com>
2019-07-18 16:30:36 -0400

    More fixes in TensorRuntime, still confused about few things there ...

--- src/runtime/TensorRuntime.cpp
+++ src/runtime/TensorRuntime.cpp
@@ -6,53 +6,72 @@ namespace runtime {
 
 void TensorRuntime::openScope(const std::string &scopeName) {
   assert(scopeName.length() > 0);
+  // complete the current scope first
+  if(currentScope.length() > 0) closeScope();
   // create new graph with name given by scope name and store it in the dags map
   auto new_pos = dags.emplace(std::make_pair(
                                scopeName,exatn::getService<TensorGraph>("boost-digraph")
                               )
                              );
-  assert(new_pos.second); // make sure there was no other scope with the same name
+  assert(new_pos.second); // to make sure there was no other scope with the same name
   currentScope = scopeName; // change the name of the current scope
   return;
 }
 
-void TensorRuntime::closeScope() {
-  //complete all operations in the current scope:
-  currentScope = "";
+
+void TensorRuntime::pauseScope() {
+  //`complete all currently executed tensor operations
+  return;
 }
 
+
+void TensorRuntime::resumeScope(const std::string &scopeName) {
+  assert(scopeName.length() > 0);
+  // pause the current scope first
+  if(currentScope.length() > 0) pauseScope();
+  //`resume the execution of the previously paused scope
+  currentScope = scopeName; // change the name of the current scope
+  return;
+}
+
+
+void TensorRuntime::closeScope() {
+  //`complete all operations in the current scope:
+  currentScope = "";
+  return;
+}
+
+
 void TensorRuntime::submit(std::shared_ptr<numerics::TensorOperation> op) {
-  // upate the output tensor executation table
+  assert(currentScope.length() > 0);
+  // upate the output tensor execution table
   auto newop_outid = op->getTensorOperandId(0);
   mtx.lock();
   auto curTableIter = outTensorExecTbl.find(currentScope);
-  if(curTableIter != outTensorExecTbl.end()) {
-    auto &cur_table = curTableIter->second;
-    if(cur_table.find(newop_outid)==cur_table.end())
-      cur_table[newop_outid]=1;
-    else
-      cur_table[newop_outid]+=1;
-  }
-  else {
-    outTensorExecTbl[currentScope][newop_outid]=1;
-  }
+  assert(curTableIter != outTensorExecTbl.end());
+  auto &cur_table = curTableIter->second;
+  if(cur_table.find(newop_outid) == cur_table.end())
+    cur_table[newop_outid] = 1;
+  else
+    cur_table[newop_outid] += 1;
   // work on graph at dags[currentScope]
   // add on to the graph
-  std::shared_ptr<TensorGraph> tg=dags[currentScope];
-  auto tg_sz=tg->size();
-  auto op1=std::make_shared<TensorOpNode>(op);
+  std::shared_ptr<TensorGraph> tg = dags[currentScope];
+  auto tg_sz = tg->size();
+  auto op1 = std::make_shared<TensorOpNode>(op);
   tg->addVertex(op1);
-  auto num_op1_operands = op->getNumOperands();
+  auto num_operands = op->getNumOperands();
   std::shared_ptr<TensorOpNode> op0;
-  for(int i=tg_sz-1; i>=0; i--) {
-    op0=tg->getVertexProperties(i);
+  for(decltype(tg_sz) i = tg_sz-1; i >= 0; i--) {
+    op0 = tg->getVertexProperties(i);
     auto op0_outid = op0->op->getTensorOperandId(0);
-    for(int j=1; j<num_op1_operands; j++) {
+    for(int j = 1; j < num_operands; j++) {
       if(op0_outid == op1->op->getTensorOperandId(j))
         tg->addEdge(op0,op1);
     }
   }
   mtx.unlock();
+  return;
 }
 
 bool TensorRuntime::sync(const numerics::TensorOperation &op, bool wait) {
--- src/runtime/TensorRuntime.hpp
+++ src/runtime/TensorRuntime.hpp
@@ -1,8 +1,35 @@
+/** ExaTN:: Tensor Runtime: Execution layer for tensor operations
+REVISION: 2019/07/18
+
+Copyright (C) 2018-2019 Tiffany Mintz, Dmitry Lyakh, Alex McCaskey
+Copyright (C) 2018-2019 Oak Ridge National Laboratory (UT-Battelle)
+
+Rationale:
+ (a) The execution space consists of one or more DAGs in which
+     nodes represent tensor operations and directed edges represent
+     dependencies between the corresponding nodes (tensor operations).
+     Each DAG is associated with a uniquely named TAProL scope.
+ (b) The DAG lifecycle:
+     openScope(name): Opens a new TAProL scope and creates its associated empty DAG.
+                      The .submit method can then be used to append new tensor
+                      operations into the current DAG. The actual execution
+                      of the submitted tensor operations may begin any time
+                      after the submission.
+     pauseScope(): Completes the execution of all started tensor operations in the
+                   current DAG and defers the execution of the rest of the DAG for later.
+     resumeScope(name): Resumes the execution of a previously paused DAG, making it current.
+     closeScope(): Completes all tensor operations in the current DAG and destroys it.
+ (c) submit(TensorOperation): Submits a tensor operation for (generally deferred) execution.
+     sync(TensorOperation): Tests the completion of a specific tensor operation.
+     sync(tensor): Tests the completion of all submitted update operations on a given tensor.
+**/
+
 #ifndef EXATN_RUNTIME_TENSORRUNTIME_HPP_
 #define EXATN_RUNTIME_TENSORRUNTIME_HPP_
 
 #include <iostream>
 #include <memory>
+#include <string>
 #include <mutex>
 
 #include "TensorGraph.hpp"
@@ -16,12 +43,25 @@ namespace runtime {
 class TensorRuntime {
 
 protected:
-  std::map<std::string, std::shared_ptr<TensorGraph>> dags; //execution graphs
-  std::string currentScope; //name of the current scope
-  std::map<std::string, std::map<std::size_t, int>> outTensorExecTbl; //table for tracking output tensor execution
-  std::mutex mtx; //mutex for locking outTensorExec and dags
+
+  /** Active execution graphs (DAGs) **/
+  std::map<std::string, std::shared_ptr<TensorGraph>> dags;
+  /** Table for tracking the execution status on a given tensor:
+  Scope name --> Scope map: Tensor Id --> Number of outstanding update operations **/
+  std::map<std::string, std::map<std::size_t, int>> outTensorExecTbl;
+  /** Name of the current scope (current DAG) **/
+  std::string currentScope;
+  /** Mutex for locking outTensorExec and dags **/
+  std::mutex mtx;
 
 public:
+
+  TensorRuntime() = default;
+  TensorRuntime(const TensorRuntime &) = delete;
+  TensorRuntime & operator=(const TensorRuntime &) = delete;
+  TensorRuntime(TensorRuntime &&) noexcept = default;
+  TensorRuntime & operator=(TensorRuntime &&) noexcept = default;
+  ~TensorRuntime() = default;
 
   /** Opens a new scope represented by a new execution graph. **/
   void openScope(const std::string & scopeName);
@@ -30,7 +70,7 @@ public:
       and pausing the further progress of the current execution graph until resume. **/
   void pauseScope();
 
-  /** Resumes the execution of the previously paused scope. **/
+  /** Resumes the execution of a previously paused scope (execution graph). **/
   void resumeScope(const std::string & scopeName);
 
   /** Closes the current scope, fully completing the current execution graph. **/
@@ -44,7 +84,7 @@ public:
   bool sync(const numerics::TensorOperation & op,
             bool wait = false);
 
-  /** Tests for completion of all outstanding tensor operations on a given tensor.
+  /** Tests for completion of all outstanding update operations on a given tensor.
       If wait = TRUE, it will block until completion. **/
   bool sync(const numerics::Tensor & tensor,
             bool wait = false);
