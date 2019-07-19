857bea5d9922084bdc266e7e472a79245df774ea
Dmitry I. Lyakh <quant4me@gmail.com>
2019-07-19 11:12:47 -0400

    Fixed build issue in TensorRuntime.

--- src/runtime/TensorRuntime.cpp
+++ src/runtime/TensorRuntime.cpp
@@ -63,14 +63,14 @@ void TensorRuntime::submit(std::shared_ptr<numerics::TensorOperation> op) {
   tg->addVertex(op1);
   auto num_operands = op->getNumOperands();
   std::shared_ptr<TensorOpNode> op0;
-  for(int j=1; j<num_op1_operands; j++) {
+  for(int j = 1; j < num_operands; j++) {
     for(decltype(tg_sz) i = tg_sz-1; i >= 0; i--) {
       op0=tg->getVertexProperties(i);
       if(op0->op->getTensorOperandId(0) == op1->op->getTensorOperandId(j)) {
         tg->addEdge(op0,op1);
         break;
-      } 
-    } 
+      }
+    }
   }
   mtx.unlock();
   return;
