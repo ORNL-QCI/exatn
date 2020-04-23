2f65ffd4da028f3eab85d1ba23db3d4efd7dd044
Dmitry I. Lyakh <quant4me@gmail.com>
2020-04-22 22:09:48 -0400

    Added more features in TensorRange class necessary for work distribution.

--- src/numerics/tensor_range.hpp
+++ src/numerics/tensor_range.hpp
@@ -1,10 +1,27 @@
 /** ExaTN::Numerics: Tensor range
-REVISION: 2020/04/19
+REVISION: 2020/04/22
 
 Copyright (C) 2018-2020 Dmitry I. Lyakh (Liakh)
 Copyright (C) 2018-2020 Oak Ridge National Laboratory (UT-Battelle) **/
 
 /** Rationale:
+ (a) Tensor range is a Cartesian product of one or more ranges.
+ (b) Each constituent range can be a subrange of a larger range,
+     thus requiring a base offset and extent for its specification.
+ (c) Each tensor range can be flattened into a single 1d super-range
+     by a standard column-wise (little endian) mapping such that
+     a unique local offset in this 1d super-range can be associated
+     with each multi-index from within the tensor range.
+ (d) If a tensor range contains at least one subrange in its definition
+     the striges must be provided during tensor range construction to
+     indicate this: A stride of index J is equal to the increment in
+     the 1d super-range associated with the parental tensor range when
+     index J is incremented by one in the current tensor range.
+     Thus, each multi-index from within the current tensor range
+     is not only associated with its local offset but also with its
+     global offset in the parental flattened 1d super-range, which
+     is equal to the local offset when the tensor range does not
+     contain subranges (tensor range = parental tensor range).
 **/
 
 #ifndef EXATN_NUMERICS_TENSOR_RANGE_HPP_
@@ -25,28 +42,37 @@ public:
                     const std::vector<DimExtent> & extents,  //in: extent of each dimension (on top of the base offset)
                     const std::vector<DimExtent> & strides); //in: stride of each dimension (global increment caused by local increment)
 
+ inline TensorRange(const std::vector<DimOffset> & bases,    //in: base offset of each dimension (0 is min)
+                    const std::vector<DimExtent> & extents); //in: extent of each dimension (on top of the base offset)
+
  TensorRange(const TensorRange &) = default;
  TensorRange & operator=(const TensorRange &) = default;
  TensorRange(TensorRange &&) noexcept = default;
  TensorRange & operator=(TensorRange &&) noexcept = default;
  ~TensorRange() = default;
 
+ /** Returns the tensor range volume (local volume). **/
+ inline DimExtent localVolume() const;
+
+ /** Returns the parental tensor range volume (global volume). **/
+ inline DimExtent globalVolume() const;
+
  /** Resets the current multi-index value to the beginning. **/
  inline void reset();
 
  /** Returns the flat offset produced by the current multi-index value per se. **/
- inline DimOffset localOffset() const;
+ inline DimOffset localOffset() const; //little endian
 
  /** Returns the flat offset produced by the current multi-index value within the global tensor range. **/
- inline DimOffset globalOffset() const;
+ inline DimOffset globalOffset() const; //based on strides
 
  /** Increments the current multi-index value.
      If the tensor range is over, return false. **/
- inline bool next();
+ inline bool next(DimOffset increment = 1);
 
  /** Decrements the current multi-index value.
      If the tensor range is over, return false. **/
- inline bool prev();
+ inline bool prev(DimOffset increment = 1);
 
 private:
 
@@ -54,6 +80,7 @@ private:
  std::vector<DimExtent> extents_;
  std::vector<DimExtent> strides_;
  std::vector<DimOffset> mlndx_;
+ DimExtent volume_;
 };
 
 
@@ -62,14 +89,54 @@ inline TensorRange::TensorRange(const std::vector<DimOffset> & bases,    //in: b
                                 const std::vector<DimExtent> & strides): //in: stride of each dimension
  bases_(bases), extents_(extents), strides_(strides), mlndx_(bases.size())
 {
- assert(bases.size() == extents.size() && bases.size() == strides.size());
+ assert(bases_.size() == extents_.size() && bases_.size() == strides_.size());
+ if(extents_.size() > 0){
+  volume_ = 1; for(const auto & extent: extents_) volume_ *= extent;
+ }else{
+  volume_ = 0;
+ }
+ reset();
 }
+
+
+inline TensorRange::TensorRange(const std::vector<DimOffset> & bases,    //in: base offset of each dimension (0 is min)
+                                const std::vector<DimExtent> & extents): //in: extent of each dimension (on top of the base offset)
+ bases_(bases), extents_(extents), strides_(bases.size()), mlndx_(bases.size())
+{
+ assert(bases_.size() == extents_.size());
+ if(extents_.size() > 0){
+  volume_ = 1;
+  for(unsigned int i = 0; i < extents_.size(); ++i){
+   strides_[i] = volume_;
+   volume_ *= extents_[i];
+  }
+ }else{
+  volume_ = 0;
+ }
+ reset();
+}
+
+
+inline DimExtent TensorRange::localVolume() const
+{
+ return volume_;
+}
+
+
+inline DimExtent TensorRange::globalVolume() const
+{
+ const auto range_rank = extents_.size();
+ if(range_rank == 0) return 0;
+ return strides_[range_rank-1] * extents_[range_rank-1];
+}
+
 
 inline void TensorRange::reset()
 {
  for(auto & ind: mlndx_) ind = 0;
  return;
 }
+
 
 inline DimOffset TensorRange::localOffset() const
 {
@@ -78,6 +145,7 @@ inline DimOffset TensorRange::localOffset() const
  return offset;
 }
 
+
 inline DimOffset TensorRange::globalOffset() const
 {
  DimOffset offset = 0;
@@ -85,14 +153,16 @@ inline DimOffset TensorRange::globalOffset() const
  return offset;
 }
 
-inline bool TensorRange::next()
+
+inline bool TensorRange::next(DimOffset increment)
 {
- int rank = mlndx_.size();
+ const int rank = mlndx_.size();
  int i = 0;
  while(i < rank){
   if(mlndx_[i] + 1 < extents_[i]){
    mlndx_[i]++;
-   return true;
+   if(--increment == 0) return true;
+   i = 0;
   }else{
    mlndx_[i++] = 0;
   }
@@ -100,14 +170,16 @@ inline bool TensorRange::next()
  return false;
 }
 
-inline bool TensorRange::prev()
+
+inline bool TensorRange::prev(DimOffset increment)
 {
- int rank = mlndx_.size();
+ const int rank = mlndx_.size();
  int i = 0;
  while(i < rank){
   if(mlndx_[i] > 0){
    mlndx_[i]--;
-   return true;
+   if(--increment == 0) return true;
+   i = 0;
   }else{
    mlndx_[i++] = extents_[i] - 1;
   }
