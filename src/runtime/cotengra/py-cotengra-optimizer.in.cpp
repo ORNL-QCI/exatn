3d5e84ae2af913539562e850b6b9cf976e9c79fb
Thien Nguyen <nguyentm@ornl.gov>
2021-03-29 16:11:16 -0400

    Add embedded python boiler-plate
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- src/runtime/cotengra/py-cotengra-optimizer.in.cpp
+++ src/runtime/cotengra/py-cotengra-optimizer.in.cpp
@@ -1,8 +1,13 @@
 #include "contraction_seq_optimizer.hpp"
 #include "cppmicroservices/BundleActivator.h"
 #include "cppmicroservices/BundleContext.h"
+#include <dlfcn.h>
+#include <pybind11/embed.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
 
 using namespace cppmicroservices;
+namespace py = pybind11;
 
 namespace exatn {
 
@@ -13,8 +18,19 @@ public:
   virtual double determineContractionSequence(
       const TensorNetwork &network, std::list<ContrTriple> &contr_seq,
       std::function<unsigned int()> intermediate_num_generator) override {
+    if (!initialized) {
+      guard = std::make_shared<py::scoped_interpreter>();
+      libpython_handle = dlopen("@PYTHON_LIB_NAME@", RTLD_LAZY | RTLD_GLOBAL);
+      initialized = true;
+    }
+
     return 0.0;
   }
+
+private:
+  bool initialized = false;
+  std::shared_ptr<py::scoped_interpreter> guard;
+  void *libpython_handle = nullptr;
 };
 
 } // namespace numerics
@@ -29,12 +45,11 @@ class US_ABI_LOCAL CotengraActivator : public BundleActivator {
 public:
   CotengraActivator() {}
 
-  /**
-   */
-  void Start(BundleContext context) {}
+  void Start(BundleContext context) {
+    context.RegisterService<exatn::numerics::ContractionSeqOptimizer>(
+        std::make_shared<exatn::numerics::ContractionSeqOptimizerCotengra>());
+  }
 
-  /**
-   */
   void Stop(BundleContext /*context*/) {}
 };
 
