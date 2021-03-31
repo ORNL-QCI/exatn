356f270f784c734bb0da9ca48cda5109b7f327ed
Dmitry I. Lyakh <quant4me@gmail.com>
2021-03-31 15:14:45 -0400

    Updated the python dependency installation script.

--- install_python_deps.sh
+++ install_python_deps.sh
@@ -1,6 +1,9 @@
 #sudo apt install libboost-all-dev
+#add --user to pip3 install if needed
+pip3 install --upgrade pip
 pip3 install setuptools wheel
-pip3 install networkx kahypar opt_einsum optuna ray autoray
+pip3 install networkx opt_einsum optuna ray autoray
+pip3 install kahypar==1.1.6
 git clone --recursive https://github.com/jcmgray/cotengra.git ./tpls/cotengra
 cd ./tpls/cotengra
 git submodule init
