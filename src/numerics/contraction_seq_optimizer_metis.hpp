419bb70be7962e9de1ffce135f63e3fe5f88219e
Dmitry I. Lyakh (Liakh) <quant4me@gmail.com>
2021-04-01 17:45:55 -0400

    Readjusted metis parameters for GTC.

--- src/numerics/contraction_seq_optimizer_metis.hpp
+++ src/numerics/contraction_seq_optimizer_metis.hpp
@@ -45,14 +45,14 @@ protected:
                              std::list<ContrTriple> & contr_seq,
                              std::function<unsigned int ()> intermediate_num_generator);
 
- static constexpr const unsigned int NUM_WALKERS = 16;
+ static constexpr const unsigned int NUM_WALKERS = 4;
  static constexpr const double ACCEPTANCE_TOLERANCE = 0.0;
 
  static constexpr const std::size_t PARTITION_FACTOR = 2;
  static constexpr const std::size_t PARTITION_MAX_SIZE = 3;
- static constexpr const std::size_t PARTITION_IMBALANCE_DEPTH = 48;
+ static constexpr const std::size_t PARTITION_IMBALANCE_DEPTH = 96;
  static constexpr const std::size_t PARTITION_GRANULARITY = PARTITION_IMBALANCE_DEPTH;
- static constexpr const double PARTITION_IMBALANCE = 1.1;
+ static constexpr const double PARTITION_IMBALANCE = 1.001;
 
  unsigned int num_walkers_;
  double acceptance_tolerance_;
