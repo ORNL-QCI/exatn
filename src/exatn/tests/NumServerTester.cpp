edd72ca673c8fcd2dc63ed5b4dcd82cd52d4745e
Dmitry I. Lyakh <quant4me@gmail.com>
2021-04-16 16:08:50 -0400

    Updated ExaTensor submodule with a fixed memory leak. Enabled all tests.

--- src/exatn/tests/NumServerTester.cpp
+++ src/exatn/tests/NumServerTester.cpp
@@ -15,7 +15,7 @@
 
 //Test activation:
 #define EXATN_TEST0
-/*#define EXATN_TEST1
+#define EXATN_TEST1
 #define EXATN_TEST2
 #define EXATN_TEST3
 #define EXATN_TEST4
@@ -34,16 +34,16 @@
 #define EXATN_TEST17
 //#define EXATN_TEST18 //buggy (parsed named spaces/subspaces)
 #define EXATN_TEST19
-//#define EXATN_TEST20 //MKL only (tensor hyper-contraction)*/
+//#define EXATN_TEST20 //MKL only (tensor hyper-contraction)
 #define EXATN_TEST21
-/*#define EXATN_TEST22
+#define EXATN_TEST22
 #define EXATN_TEST23
 #define EXATN_TEST24
 #define EXATN_TEST25
 #define EXATN_TEST26
 #define EXATN_TEST27
 #define EXATN_TEST28
-#define EXATN_TEST29*/
+#define EXATN_TEST29
 
 
 #ifdef EXATN_TEST0
@@ -2283,7 +2283,7 @@ TEST(NumServerTester, neurIPS) {
   const exatn::DimExtent chi1 = 3; //Laptop: 3; Summit (1 node): 4
   const auto chi2 = std::min(chi1*chi1,512ULL);
   const auto chi3 = std::min(chi2*chi2,1024ULL);
-  const auto chi4 = std::min(chi3*chi3,4096ULL);
+  const auto chi4 = std::min(chi3*chi3,2048ULL);
   success = exatn::createTensor("Z",TENS_ELEM_TYPE,TensorShape{chi3,chi3,chi4}); assert(success);
   success = exatn::createTensor("A",TENS_ELEM_TYPE,TensorShape{chi4,chi4}); assert(success);
   success = exatn::createTensor("B",TENS_ELEM_TYPE,TensorShape{chi3,chi3,chi4}); assert(success);
